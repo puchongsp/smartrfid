b92c5f22f2c5ac755128d85eae8a451431d7cfbc
luc.we89 <luc.we89@gmail.com>
2013-06-28 13:43:43 +0000

    Ticket merged

--- SmartRFID/src/sushil/luc/ticket/Ticket.java
+++ SmartRFID/src/sushil/luc/ticket/Ticket.java
@@ -1,9 +1,11 @@
 package sushil.luc.ticket;
 
 import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
 
 import sushil.luc.item.Item;
+import sushil.luc.item.ItemStatus;
 import sushil.luc.msc.Client;
 
 public class Ticket {
@@ -16,7 +18,8 @@ public class Ticket {
 	
 	public Ticket()
 	{
-		
+		this.Items = new LinkedList<Item>();
+		fetchAllItems();
 	}
 	
 	private void fetchAllItems()
@@ -24,12 +27,49 @@ public class Ticket {
 		
 	}
 	
-	private void PlanIndoorRoute()
+	/**
+	 * Checks if the given RFID is contained in this ticket 
+	 * @param check
+	 * @return true if the RFID is contained, false if not
+	 */
+	private boolean checkItem(String check)
 	{
+		boolean result = false;
 		
+		int i =0;
+		while (i<this.Items.size() && !result)
+		{
+			if (this.Items.get(i).getRFID().equals(check))
+			{
+				result =true;
+				this.Items.get(i).setStatus(ItemStatus.COLLECTED);
+			}
+			i++;
+		}
+		
+		return result;
 	}
 	
+
+	/**
+	 * Checks if the given RFID is contained in the current Itemlist of the Ticket
+	 * @param RFID
+	 */
+	public void checkRFIDInTicket (String RFID)
+	{
+		if (checkItem(RFID))
+		{
+			System.out.println("Positive Feedback");
+		}
+		else
+		{
+			System.out.println("Negative Feedback");
+		}
+	}
+	
+	
 	public long getTicketID() {
+
 		return TicketID;
 	}
 
