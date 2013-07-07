10f56e692dd58e46e797ec03e8cf2330f0dcd0c9
luc.we89 <luc.we89@gmail.com>
2013-07-07 08:59:55 +0000

    Item Info Tab now working

--- SmartRFID/src/sushil/luc/gui/ItemInfoFragment.java
+++ SmartRFID/src/sushil/luc/gui/ItemInfoFragment.java
@@ -2,18 +2,67 @@ package sushil.luc.gui;
 
 import sushil.luc.smartrfid.R;
 import android.app.Fragment;
+import android.opengl.Visibility;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class ItemInfoFragment extends Fragment {
-
-
+	
+	private TextView info;
+	private TextView itemid;
+	private TextView itemname;
+	private TextView itemrfid;
+	private TextView itemstatus;
+	private TextView itemlocation;
+	
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.new_item_fragment, container, false);
+    	
+    	View v = inflater.inflate(R.layout.item_info_fragment, container, false);
+    	
+    	info = (TextView) v.findViewById(R.id.InfoText);
+    	itemid = (TextView) v.findViewById(R.id.item_id);
+    	itemname = (TextView) v.findViewById(R.id.item_name);
+    	itemrfid = (TextView) v.findViewById(R.id.item_rfid);
+    	itemstatus = (TextView) v.findViewById(R.id.item_status);
+    	itemlocation = (TextView) v.findViewById(R.id.item_location);
+    	
+    	itemid.setVisibility(View.INVISIBLE);
+    	itemname.setVisibility(View.INVISIBLE);
+    	itemrfid.setVisibility(View.INVISIBLE);
+    	itemstatus.setVisibility(View.INVISIBLE);
+    	itemlocation.setVisibility(View.INVISIBLE);
+    	
+        return v;
+    }
+    
+/*	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+    	
+	}*/
+    
+    public void displayInfo (String ItemId, String ItemName, String ItemRfid, String ItemStatus, String ItemLocation)
+    {
+    	
+    	itemid.setVisibility(View.VISIBLE);
+    	itemname.setVisibility(View.VISIBLE);
+    	itemrfid.setVisibility(View.VISIBLE);
+    	itemstatus.setVisibility(View.VISIBLE);
+    	itemlocation.setVisibility(View.VISIBLE);
+    	
+    	info.setText("Last scanned Item :");
+    	itemid.setText("Item Id: "+ItemId);
+    	itemname.setText("Item Name: "+ItemName);
+    	itemrfid.setText("Item RFID: "+ItemRfid);
+    	itemstatus.setText("Status: "+ItemStatus);
+    	itemlocation.setText("Item Location: "+ItemLocation);
     }
 	
 	
