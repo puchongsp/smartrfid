5002492b3d53980def1e72a9fc5e253c3a860c2d
sushilman <sushilman@gmail.com>
2013-09-02 01:32:45 +0000

    updating documentation

--- SmartRFID/src/sushil/luc/network/MIMETypes.java
+++ SmartRFID/src/sushil/luc/network/MIMETypes.java
@@ -1,5 +1,10 @@
 package sushil.luc.network;
 
+/**
+ * MIME types that web service API is going to return
+ * either JSON or XML
+ * in our case, we are using JSON
+ */
 public enum MIMETypes {
 	
 	APPLICATION_JSON("application/json");
